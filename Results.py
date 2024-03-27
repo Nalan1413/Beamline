c5fbf2fc4ab4f9e95a7c6afd5f5e05c984e2a465
Nalan <148653969+Nalan1413@users.noreply.github.com>
2024-03-27 15:27:18 +0100

    Update Results.py

--- Results.py
+++ Results.py
@@ -2,8 +2,16 @@
 import pickle
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
+import pschitt.emission as em
+import pschitt.vizualisation as viz
+import pschitt.sky_objects as sky
+import pschitt.geometry as geo
+import pschitt.camera_image as ci
+import numpy as np
+import matplotlib.pyplot as plt
+import math
 
-with open('data.pkl', 'rb') as f:
+with open('new_data.pkl', 'rb') as f:
     Primary_shower_data = pickle.load(f)
     Secondary_shower_data = pickle.load(f)
 (particle_x, particle_y, particle_z, particle_en,
@@ -16,11 +24,12 @@ with open('data.pkl', 'rb') as f:
     (Secondary_shower_data["sec_particle_x"], Secondary_shower_data["sec_particle_y"], Secondary_shower_data["sec_particle_z"], Secondary_shower_data["sec_particle_en"],
      Secondary_shower_data["sec_parent_ids_m1"], Secondary_shower_data["sec_parent_ids_m2"], Secondary_shower_data["sec_particle_eta"], Secondary_shower_data["sec_particle_phi"])
 
-shower_color = '#151340'
-sec_color = '#5fa8d9'
+shower_color = 'black'
+sec_color = '#d18580'
 shower_colors = [shower_color] * len(particle_x)
 sec_colors = [sec_color] * sum(len(particles) for particles in sec_particle_x)
 
+"""
 fig, ax_polar = plt.subplots(1, 1, subplot_kw=dict(projection='polar'), figsize=(10, 8))
 for i in range(len(particle_x)):
     ax_polar.scatter(particle_phi[i], particle_eta[i], c=particle_en[i], cmap='viridis', s=50, alpha=0.7)
@@ -44,10 +53,11 @@ for j in range(len(sec_particle_x)):
 ax_polar.set_title('Polar Coordinates')
 cbar_polar = plt.colorbar(ax_polar.collections[0], ax=ax_polar, orientation='vertical')
 cbar_polar.set_label('Energy')
+"""
 
 fig = plt.figure(figsize=(10, 8))
 ax_3d = fig.add_subplot(111, projection='3d')
-ax_3d.scatter(particle_x, particle_y, particle_z, c=shower_colors, s=50, alpha=0.7)
+ax_3d.scatter(particle_x, particle_y, particle_z, c=particle_en, s=50, alpha=0.7)
 
 for i in range(len(particle_x)):
     if parent_ids_m1[i] != 0:
@@ -60,9 +70,9 @@ for i in range(len(particle_x)):
                    [particle_z[parent_ids_m2[i]], particle_z[i]], color='gray', linestyle='--')
 
 for j in range(len(sec_particle_x)):
-    ax_3d.scatter(sec_particle_x[j], sec_particle_y[j], sec_particle_z[j], c=sec_colors[j], s=50, alpha=0.7)
+    ax_3d.scatter(sec_particle_x[j], sec_particle_y[j], sec_particle_z[j], c=sec_particle_en[j], s=50, alpha=0.7)
 
-    for k in range(len(sec_particle_x[0])):
+    for k in range(len(sec_particle_x[j])):
         if sec_parent_ids_m1[j][k] != 0:
             ax_3d.plot([sec_particle_x[j][sec_parent_ids_m1[j][k]], sec_particle_x[j][k]],
                        [sec_particle_y[j][sec_parent_ids_m1[j][k]], sec_particle_y[j][k]],
@@ -71,22 +81,54 @@ for j in range(len(sec_particle_x)):
             ax_3d.plot([sec_particle_x[j][sec_parent_ids_m2[j][k]], sec_particle_x[j][k]],
                        [sec_particle_y[j][sec_parent_ids_m2[j][k]], sec_particle_y[j][k]],
                        [sec_particle_z[j][sec_parent_ids_m2[j][k]], sec_particle_z[j][k]], color='gray', linestyle='--')
-
 ax_3d.set_xlabel('X')
 ax_3d.set_ylabel('Y')
 ax_3d.set_zlabel('Z')
 ax_3d.set_title('3D Coordinates')
 
-ax_3d.set_xlim(-0.005, 0.005)
-ax_3d.set_ylim(-0.005, 0.005)
-ax_3d.set_zlim(-0.05, 0.05)
-"""
+
 cbar_ax_3d = plt.colorbar(ax_3d.collections[0], ax=ax_3d, orientation='vertical')
 cbar_ax_3d.set_label('Color')
-"""
+
+
+ax_3d.set_xlim(-25, 25)
+ax_3d.set_ylim(-25, 25)
+ax_3d.set_zlim(-25, 25)
 
 print(f"Number of primary shower particles{len(particle_x)}")
 num_points = len(particle_x) + sum([len(particles) for particles in sec_particle_x])
 print(f"Number of shower particles: {num_points}")
 
+par_x = [x for x in particle_x]
+for sec_x in sec_particle_x:
+    par_x.extend(sec_x)
+par_y = [y for y in particle_y]
+for sec_y in sec_particle_y:
+    par_y.extend(sec_y)
+par_z = [z for z in particle_z]
+for sec_z in sec_particle_z:
+    par_z.extend(sec_z)
+shower = sky.shower()
+shower.particles = np.array(list(zip(par_x, par_y, par_z)))
+
+talt = math.radians(270)
+taz = math.radians(0)
+tel_normal = geo.altaz_to_normal(talt, taz)
+
+tel1 = geo.Telescope([1, 1, 50], tel_normal)
+tel2 = geo.Telescope([-1, 1, 50], tel_normal)
+tel3 = geo.Telescope([-1, -1, 50], tel_normal)
+tel4 = geo.Telescope([1, -1, 50], tel_normal)
+tel5 = geo.Telescope([0, 0, 50], tel_normal)
+
+altel = [tel1, tel2, tel3, tel4, tel5]
+
+noise = 0
+ci.array_shower_imaging(shower, altel, noise)
+fig, axes = plt.subplots(1, len(altel), figsize=(20, 3))
+for tel, ax in zip(altel, axes):
+    ax = viz.display_camera_image(tel, s=4, ax=ax)
+    ax.set_title("Signal sum = {}".format(tel.signal_hist.sum()))
+    ax.set_xticks([])
+    ax.set_yticks([])
 plt.show()
